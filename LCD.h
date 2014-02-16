eefafefd970e763917c054c6948c87f08d34f968
Massimo Nevi <maxnevi@gmail.com>
2014-02-16 17:36:01 +0100

    Create LCD.h

---
+++ LCD.h
@@ -0,0 +1,35 @@
+void Riga20(){
+  lcd.print(F("                    "));
+}
+
+void Riga16(){
+  lcd.print(F("                "));
+}
+
+void Riga10(){
+  lcd.print(F("          "));
+}
+
+void Riga8(){
+  lcd.print(F("        "));
+}
+
+void Riga5(){
+  lcd.print(F("     "));
+}
+
+void Riga4(){
+  lcd.print(F("    "));
+}
+
+void Riga3(){
+  lcd.print(F("   "));
+}
+
+void Riga2(){
+  lcd.print(F("  "));
+}
+
+void Riga1(){
+  lcd.print(F(" "));
+}
