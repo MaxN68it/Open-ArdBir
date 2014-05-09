59bfc5916626c0dd559b347cec49da92ac3282bb
Massimo Nevi <maxnevi@gmail.com>
2014-05-09 10:24:16 +0200

    Update Funzioni.h

--- Funzioni.h
+++ Funzioni.h
@@ -1,12 +1,17 @@
 void CountDown(unsigned long Tempo, byte posX, byte posY, byte numH){
-  byte Ore, Minuti, Secondi;
+  //numH = 1 Ore a 1 cifra
+  //numH = 2 Ore a 2 cifre
   
-  Ore= byte(Tempo/3600);
-  Minuti= byte((Tempo-(Ore*3600))/60);
-  Secondi= byte(Tempo-((Ore*3600)+(Minuti*60)));
-     
+  byte Ore, Minuti, Secondi;
+ 
+  Ore= (byte)(Tempo/3600);
+  Minuti= (byte)((Tempo%3600)/60);
+  Secondi= (byte)(Tempo%60);
+  
   lcd.setCursor(posX, posY);
-  if (numH==2)lcd.print(F("0"));
+  if (numH==2){
+    if(Ore<10)lcd.print(F("0"));
+  }
   lcd.print(Ore);
       
   if(Minuti<10)lcd.print(F(":0"));
@@ -34,32 +39,31 @@ byte btn_Press (byte Button_press, int msTime){
 // repeat button press
 byte btn_Repeat (byte Button_press){
   if (digitalRead(Button_press)==0){
-    delay(225);
+    delay(250);
     return 1;
   }
   return 0;
 }
 
 byte LeggiPulsante(byte& Verso, unsigned long& Timer ){	
+  // Verso=1 UP
+  // Verso=2 DWN
+  
   boolean f_btnUp,f_btnDn;
   
   if (digitalRead (Button_up)==0){	//Pressione specifica del pulsante UP
     if(Verso!=1)Timer=millis();		//Se non esiste pressione precedente parte il conteggio del tempo
     f_btnUp=true;
     Verso=1;		//Sentinella pulsante premuto
-    //Set_1(Set, Up, Low, Step, Timer, Verso);	
-    delay(25);
+    delay(35);
   }else f_btnUp=false;
   
   if (digitalRead (Button_dn)==0){	//Pressione specifica del pulsante UP
     if(Verso!=2)Timer=millis();		//Se non esiste pressione precedente parte il conteggio del tempo
     f_btnDn=true;
     Verso=2;		//Sentinella pulsante premuto
-    //Set_1(Set, Up, Low, Step, Timer, Verso);	
-    delay(25); 
+    delay(35); 
   }else f_btnDn=false;
-  
-  //delay(50);
   
   if(digitalRead (Button_up)==0)f_btnUp=true;	//Legge lo stato del pulsante allâ€™uscita per aggiornare il flag
   else f_btnUp=false;
@@ -73,22 +77,22 @@ byte LeggiPulsante(byte& Verso, unsigned long& Timer ){
 
 
 float Arrotonda025(float& Num){
-  int Appoggio;  
   // Appoggio la parte intera
-  Appoggio = (int)int(Num);           
+  int Appoggio= (int)Num;
+  
   // Arrotondo il valore con peso 0.25
   Num=Appoggio+int((Num-Appoggio)*1000/225)*0.25;
 }
 
 float ConvertiCtoF(float& Num){        
   Num = Num/16;              // Recupero il valore
-  Num = (Num*1.8)+32;          // Converto in Â°F
+  Num = (Num*1.8)+32;          // Converto in °F
   Arrotonda025(Num);
   Num = Num*16;              // Preparo il valore per la registrazione
 }
 float ConvertiFtoC(float& Num){
   Num = Num/16;              // Recupero il valore
-  Num = (Num-32)/1.8;            // Converto in Â°C
+  Num = (Num-32)/1.8;            // Converto in °C
   Arrotonda025(Num);
   Num = Num*16;              // Preparo il valore per la registrazione
 }
@@ -102,13 +106,9 @@ int Set(int& Set, int Up, int Low, int Step, long Timer, byte Verso){
   	
   delay(35);
   if(Verso==1){
-//    if(Set<Low)Set=Low;
-//    if(Set>=Up)Set=Up;
     ControllaPulsante=digitalRead(Button_up);
   }
   if(Verso==2){
-//    if(Set>Up)Set=Up;
-//    if(Set<=Low)Set=Low;
     ControllaPulsante=digitalRead(Button_dn);
   }
   
@@ -137,13 +137,9 @@ float Set(float& Set, float Up, float Low, float Step, long Timer, byte Verso){
   	
   delay(35);
   if(Verso==1){
-//    if(Set<Low)Set=Low;
-//    if(Set>=Up)Set=Up;
     ControllaPulsante=digitalRead(Button_up);
   }
   if(Verso==2){
-//    if(Set>Up)Set=Up;
-//    if(Set<=Low)Set=Low;
     ControllaPulsante=digitalRead(Button_dn);
   }
   
@@ -172,13 +168,9 @@ byte Set(byte& Set, byte Up, byte Low, byte Step, long Timer, byte Verso){
   
   delay(35);
   if(Verso==1){
-//    if(Set<Low)Set=Low;
-//    if(Set>=Up)Set=Up;
     ControllaPulsante=digitalRead(Button_up);
   }
   if(Verso==2){
-//    if(Set>Up)Set=Up;
-//    if(Set<=Low)Set=Low;
     ControllaPulsante=digitalRead(Button_dn);
   }
   
